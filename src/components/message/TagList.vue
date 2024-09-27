ee3bab1179ae0a88d7fd7490413d540c86ed889d
4o4E <869951226@qq.com>
2024-09-27 17:19:57 +0800

    feat: 使用确切的px替换em

--- src/components/message/TagList.vue
+++ src/components/message/TagList.vue
@@ -1,7 +1,10 @@
 <template>
   <div style="padding: 2px">
     <template v-for="tag of props.tags" :key="tag">
-      <el-tag type="primary" :size="props.size ?? 'large'" style="margin: .1em .3em">{{ tagInfo.tagsMap[tag]?.name ?? tag }}</el-tag>
+      <el-tag
+        type="primary"
+        :size="props.size ?? 'large'" style="margin: 2px 5px"
+      >{{ tagInfo.tagsMap[tag]?.name ?? tag }}</el-tag>
     </template>
   </div>
 </template>

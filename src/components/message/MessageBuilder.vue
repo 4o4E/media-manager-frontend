4bcb976a62e0a2448de341243b50fd58fbc96026
4o4E <869951226@qq.com>
2024-10-16 17:59:08 +0800

    fix: 文本中没有换行时的异常

--- src/components/message/MessageBuilder.vue
+++ src/components/message/MessageBuilder.vue
@@ -52,7 +52,7 @@
                 />
               </corner-icon>
               <div v-if="message.type === 'TEXT'" style="margin-bottom: 8px; display: flex;">
-                <el-input type="textarea" disabled :rows="[...(message as UnUploadTextMessage).content.match(/\n/g)].length + 1" v-model="(message as UnUploadTextMessage).content" />
+                <el-input type="textarea" disabled :rows="[...(message as UnUploadTextMessage).content.match(/\n/g) ?? []].length + 1" v-model="(message as UnUploadTextMessage).content" />
                 <el-button size="small" icon="Close" circle @click="data.splice(index, 1)" style="margin-left: 5px;" />
               </div>
             </li>

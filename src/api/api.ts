d74e7a9a8618a9fe30d53646f3ead30dcc8bc6e2
4o4E <869951226@qq.com>
2024-10-21 11:34:15 +0800

    fix: token不动态获取

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,18 +2,28 @@ import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import { auth } from '@/api/auth'
 
-// 后端api地址
-export const baseUrl = import.meta.env.VITE_APP_BASE_URL
-
 /**
  * 配置好的axios实例
  */
-export const client = axios.create({
-  // baseURL: baseUrl,
-  timeout: 1000,
-  headers: { Authorization: auth.value?.token },
-  validateStatus: status => status < 500,
-})
+export const client = (() => {
+   const instance = axios.create({
+    timeout: 1000,
+    validateStatus: status => status < 500,
+  })
+  // 拦截器
+  instance.interceptors.request.use(
+    config => {
+      // 动态获取 auth 值并设置 Authorization 头部
+      const token = auth.value?.token
+      if (token) config.headers.Authorization = token
+      return config;
+    },
+    error => {
+      return Promise.reject(error);
+    }
+  );
+   return instance
+})()
 
 export async function uploadFile(blob: Blob): Promise<string | null> {
   const formData = new FormData()

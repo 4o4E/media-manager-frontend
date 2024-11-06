5d3f1c152bb8d57d955d9d9876a69199743cf50b
4o4E <869951226@qq.com>
2024-11-06 18:04:02 +0800

    feat: 优化文件上传

--- src/components/message/choose/ChooseFile.vue
+++ src/components/message/choose/ChooseFile.vue
@@ -17,27 +17,58 @@
     />
   </corner-icon>
   <!-- 上传文件 -->
-  <el-upload
-    v-else
-    :accept="choose[chooseType].accept"
-    :auto-upload="false"
-    :limit="1"
-    :on-exceed="handleExceed"
-    :on-change="onChange"
-    :style="`maxWidth: ${maxWidth}px`"
-  >
-    <el-button>
-      <el-text style="width: 300px; margin: 0 auto;">选择{{ choose[chooseType].display }}</el-text>
-    </el-button>
-  </el-upload>
+  <div v-else>
+    <div
+      ref="uploadZone"
+      class="upload-zone"
+      @dragenter="fileDragenter"
+      @dragover="e => e.preventDefault()"
+      @dragleave="fileDragleave"
+      @drop="fileDrop"
+    >
+      <div style="display: block;">
+        <div style="display: flex; justify-content: center;">
+          <span>将文件拖拽至此</span>
+          <template v-if="over">
+            <span>，松开鼠标开始上传</span>
+          </template>
+          <template v-else>
+            <span>，或</span>
+            <label>
+              <span style="color: #11A8FF; cursor: pointer;">点此上传</span>
+              <input
+                type="file"
+                ref="fileInputInstance"
+                style="display: none;"
+                :accept="choose[chooseType].accept.map(e => '.' + e).join(', ')"
+                @change="onFileSelect"
+              />
+            </label>
+            <br />
+          </template>
+        </div>
+        <span>支持 {{ choose[chooseType].accept.join(', ') }} 等格式</span>
+      </div>
+    </div>
+    <!--    <el-upload-->
+    <!--      :accept="choose[chooseType].accept"-->
+    <!--      :auto-upload="false"-->
+    <!--      :limit="1"-->
+    <!--      :on-exceed="handleExceed"-->
+    <!--      :on-change="onChange"-->
+    <!--      :style="`maxWidth: ${maxWidth}px`"-->
+    <!--    >-->
+    <!--      <el-button>-->
+    <!--        <el-text style="width: 300px; margin: 0 auto;">选择{{ choose[chooseType].display }}</el-text>-->
+    <!--      </el-button>-->
+    <!--    </el-upload>-->
+  </div>
 </template>
 
 <script setup lang="ts">
 
-import { ref } from 'vue'
-import type { UploadFile, UploadInstance, UploadRawFile } from 'element-plus'
-import { genFileId } from 'element-plus'
-import { requireAuth } from '@/api/auth'
+import { onMounted, onUnmounted, ref } from 'vue'
+import { ElMessage } from 'element-plus'
 import type { HasLength, HasSize } from '@/api/type'
 import CornerIcon from '@/components/CornerIcon.vue'
 
@@ -49,24 +80,90 @@ interface PropsType {
 const choose = {
   IMAGE: {
     display: '图片',
-    accept: '.png, .jpg, .jpeg, .gif, .bmp',
+    accept: ['png', 'jpg', 'jpeg', 'gif', 'bmp'],
   },
   VIDEO: {
     display: '视频',
-    accept: '.mp4, .avi',
+    accept: ['mp4', 'avi'],
   },
   AUDIO: {
     display: '音频',
-    accept: '.mp3, .flac, .m4a, .ogg',
+    accept: ['mp3', 'flac', 'm4a', 'ogg'],
   },
 }
 
-defineProps<PropsType>()
-const upload = ref<UploadInstance>()
-const file = ref<UploadFile>()
+const props = defineProps<PropsType>()
+const file = ref<File>()
 const fileUrl = ref<string>()
+const uploadZone = ref<HTMLElement>()
+const fileInputInstance = ref<HTMLElement>()
+const over = ref()
 
-requireAuth()
+function onFileSelect() {
+  const files = fileInputInstance.value.files
+  if (files.length === 0) return
+  handleFileSelect(files[0])
+}
+
+function fileDrop(e) {
+  e.preventDefault()
+  over.value = undefined
+  unHighlight(e)
+  handleFileSelect(e.dataTransfer.files[0])
+}
+
+function handleFileSelect(selectedFile: File) {
+  const format = selectedFile.name.substring(selectedFile.name.lastIndexOf('.') + 1)
+  console.log(format)
+  const chooseElement = choose[props.chooseType]
+  if (!chooseElement.accept.includes(format)) {
+    ElMessage({
+      type: 'warning',
+      message: `文件格式不正确, ${chooseElement.display}仅支持 ${chooseElement.accept.join(', ')} 等格式`,
+    })
+    return
+  }
+  metaInfo.value.format = format
+  file.value = selectedFile
+  fileUrl.value = URL.createObjectURL(selectedFile)
+}
+
+function fileDragenter(e) {
+  e.preventDefault()
+  over.value = e.target
+}
+
+function fileDragleave(e) {
+  if (e.target !== over.value) return
+  e.preventDefault()
+  over.value = undefined
+  e.preventDefault()
+}
+
+const hl = ref()
+
+function highlight(e) {
+  hl.value = e.target
+  uploadZone.value!.classList.add('active')
+}
+
+function unHighlight(e) {
+  if (e.target !== hl.value) return
+  hl.value = undefined
+  uploadZone.value!.classList.remove('active')
+}
+
+onMounted(() => {
+  document.addEventListener('dragenter', highlight)
+  document.addEventListener('dragleave', unHighlight)
+  document.addEventListener('dragdrop', unHighlight)
+})
+
+onUnmounted(() => {
+  document.removeEventListener('dragenter', highlight)
+  document.removeEventListener('dragleave', unHighlight)
+  document.removeEventListener('dragdrop', unHighlight)
+})
 
 const metaInfo = ref<HasLength & HasSize>({})
 
@@ -79,39 +176,19 @@ function clear() {
 function handleVideoMetadata(event: Event) {
   const target = event.target as HTMLVideoElement
   if (target) {
-    metaInfo.value = {
-      length: target.duration,
-      width: target.videoWidth,
-      height: target.videoHeight,
-    }
+    metaInfo.value.length = target.duration
+    metaInfo.value.width = target.videoWidth
+    metaInfo.value.height = target.videoHeight
   }
 }
 
 function handleImageMetadata(event: Event) {
   const target = event.target as HTMLImageElement
   if (target) {
-    metaInfo.value = {
-      width: target.naturalWidth,
-      height: target.naturalHeight,
-    }
+    metaInfo.value.width = target.naturalWidth
+    metaInfo.value.height = target.naturalHeight
   }
 
-}
-
-// 用于限制只上传一个文件
-function handleExceed(files) {
-  console.log(upload.value)
-  upload.value!.clearFiles()
-  const file = files[0] as UploadRawFile
-  file.uid = genFileId()
-  metaInfo.value.format = file.name.substring(file.name.lastIndexOf('.'))
-  upload.value!.handleStart(file)
-}
-
-// 用来捕获上传的文件
-function onChange(uploadFile: UploadFile) {
-  file.value = uploadFile
-  fileUrl.value = URL.createObjectURL(uploadFile.raw!)
 }
 
 defineExpose({
@@ -122,5 +199,21 @@ defineExpose({
 })
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.upload-zone {
+  border: 1px #777 dashed;
+  padding: 10px;
+  border-radius: 5px;
+  min-height: 300px;
+  min-width: 400px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+}
+
+.upload-zone.active {
+  background: #409eff77;
+  border-color: #409eff;
+}
 </style>
